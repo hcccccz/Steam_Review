b09f8fb1292d2b534e69498bce750831aa5c88cf
hc <0903006293asd@gmail.com>
2021-09-07 17:07:52 +0800

    asd

--- discount.py
+++ discount.py
@@ -6,6 +6,59 @@ import re
 import time
 api_key = "cde859f3f3547c8bf77941b2cff1e214884d9d4b"
 
+def average(l):
+    if len(l):
+        return sum(l)/len(l)
+    else:
+        return 0
+
+
+def soup_process(soup):
+    soup = soup.find_all("div",{"class":"lg2 game","data-shop":"steam"})
+    cut_time = 0
+    average_cut = []
+    average_cut_duration = []
+    average_price = []
+
+    for s in soup:
+        pattern = re.compile(r'\d+')
+        pattern_price = re.compile(r'\$(\d+\.\d+?)<')
+        price_group = [eval(i) for i in pattern_price.findall(str(s))]
+        min_price = min(price_group)
+        max_price = max(price_group)
+        duration = s.find("i").get_text() #singe duration
+
+        if max_price == 0:
+            free = True
+        else:
+            free = False
+            if min_price < max_price: #it is a discount
+                cut_time += 1 #cut time
+                cut_off = 1 - min_price/max_price
+                average_cut.append(cut_off) #average_cut
+                match = pattern.match(duration)
+                if match:
+                    day = match.group()
+                else:
+                    day = 1
+                average_cut_duration.append(eval(day)) #average_cut_duration
+                average_price.append(min_price) #average_price
+            else:
+                average_price.append(min_price)
+    price_p = sum(average_price)
+    if price_p != 0:
+
+        c = cut_time
+        a = average(average_cut)
+        b= average(average_cut_duration)
+        d = average(average_price)
+        print("cut_time: {},average_cut: {}, duration: {}, average_price: {}".format(c,a,b,d))
+        time.sleep(2)
+    else:
+        print("this game is free")
+
+
+
 redis = StrictRedis(password="2921038")
 
 keys = [ i.decode("utf-8") for i in redis.keys()]
@@ -21,65 +74,32 @@ with open("plain.json") as file:
 plain_map = json.loads(plain)['data']['steam']
 #
 
-# for key in keys:
-#
-#     free = False
-#     id = "app/" +key
-#     if id in plain_map:
-#         data = json.loads(redis.get(key).decode("utf-8"))
-#         plain = plain_map[id]
-#         url = "https://isthereanydeal.com/game/" +plain + "/history/"
-#         #https://isthereanydeal.com/game/battlecrewspacepirates/history/
-#
-#         r = requests.get(url)
-#         soup = BeautifulSoup(r.text,"html.parser")
-#         soup = soup.find("div",{"id":"historyLogContent"})
-#         soup = soup.find_all("div",{"class":"lg2 game","data-shop":"steam"})
-#
-#         cut_time = 0
-#         average_cut = []
-#         average_cut_duration = []
-#         average_price = []
-#         print(plain)
-#         for s in soup:
-#             pattern = re.compile(r'\d+')
-#             duration = s.find("i").get_text()
-#
-#             initial_price = s.find("span",{"class":"lg2__price"}).get_text()
-#             initial_price = eval(initial_price.replace("$",""))
-#             after_price = s.find("span",{"lg2__price lg2__price--new"}).get_text()
-#             after_price= eval(after_price.replace("$",""))
-#
-#             if initial_price == 0:
-#                 free = True
-#             else:
-#                 if after_price < initial_price:
-#                     cut_time += 1 #cut time
-#                     cut_off = 1 - after_price/initial_price
-#                     average_cut.append(cut_off) #average_cut
-#                     day = pattern.match(duration).group()
-#                     average_cut_duration.append(eval(day)) #average_cut_duration
-#                     average_price.append(after_price) #average_price
-#                 else:
-#                     average_price.append(after_price)
-#         if not free:
-#
-#             c = cut_time
-#             a = sum(average_cut)/len(average_cut)
-#             b= sum(average_cut_duration)/len(average_cut_duration)
-#             d = sum(average_price)/len(average_price)
-#             print("cut_time: {},average_cut: {}, duration: {}, average_price".format(c,a,b,d))
-#             time.sleep(2)
+for idx in range(len(keys)):
 
-r = requests.get("https://isthereanydeal.com/game/battlecrewspacepirates/history/")
-soup = BeautifulSoup(r.text,"html.parser")
-soup = soup.find("div",{"id":"historyLogContent"})
-soup = soup.find_all("div",{"class":"lg2 game","data-shop":"steam"})
-pattern = re.compile(r'\d+')
-pattern_price = re.compile(r'\$(\d+\.\d+?)<')
-for i in soup:
-    day = i.find("i").get_text() #duration
-    match = pattern.match(day)
-    # if match:
-        # print(match.group())
-    print(pattern_price.search(str(i)).group(1))
+    free = False
+    id = "app/" +keys[idx]
+    if id in plain_map:
+        data = json.loads(redis.get(keys[idx]).decode("utf-8"))
+        plain = plain_map[id]
+        url = "https://isthereanydeal.com/game/" +plain + "/history/"
+        #https://isthereanydeal.com/game/driftgearracingfree/history/
+
+        print(idx,":",plain)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,"html.parser")
+        soup = soup.find("div",{"id":"historyLogContent"})
+
+        if soup:
+            soup_process(soup)
+        else:
+            print("no data")
+
+
+
+
+
+#
+# r = requests.get("https://isthereanydeal.com/game/dungeonfighteronline/history/")
+# soup = BeautifulSoup(r.text,"html.parser")
+# soup = soup.find("div",{"id":"historyLogContent"})
+# soup_process(soup)
