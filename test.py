481e8ab3fdc89c9ce3d532024403e0e5577bf370
hc <0903006293asd@gmail.com>
2021-09-08 13:19:29 +0800

    l

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
-from redis import StrictRedis
-import math
+
+#删掉 "average achievements"
+#修改 “price“
+#算出推出日期
+#算出评论数量
+#算出好评率
+#增加语言支持
+
 import json
-redis = StrictRedis(password="2921038")
-d = redis.get("717840")
-d = json.loads(d.decode("utf-8"))
-print(json.dumps(d,indent=4))
+
+def remove_key(dict_ob,key):
+    if key in dict_ob:
+        del dict_ob[key]
+    return dict_ob
+
+with open("before_clean.json", "r") as file:
+    before = json.loads(file.read())
+    before = remove_key(before,"average achievements")
