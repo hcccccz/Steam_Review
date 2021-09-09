c30321f2f5dd78e5f455252b0dea22603473052c
hcccccz <40889071+hcccccz@users.noreply.github.com>
2021-09-10 00:04:03 +0800

    u

--- discount.py
+++ discount.py
@@ -69,7 +69,7 @@ print(len(keys))
 for key in keys:
     item = json.loads(redis.get(key).decode("utf-8"))
     if len(item) == 33 or len(item) ==1:
-        print(len(item))
+        # print(len(item))
         keys.remove(key)
 print(len(keys))
 # i = json.loads(i)
