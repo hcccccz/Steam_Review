cab251ded93135a1fadf4e257df839ba1a955a53
hc <0903006293asd@gmail.com>
2021-10-22 09:17:32 +0800

    Asd

--- rSCRIPT.R
+++ rSCRIPT.R
@@ -2,11 +2,9 @@ install.packages(car)
 library(dplyr)
 library(car)
 library(ggplot2)
-setwd("~/repo/Steam_review/")
-
-data = read.table("Steam1.csv",header=TRUE,quote="\"",fill=TRUE,sep=",")
-
-
+library(ggdark)
+setwd("Steam_review")
+data = read.table("Steam_oct18.csv",header=TRUE,quote="\"",fill=TRUE,sep=",")
 
 
 data$average.total.playtime <- sub("h","",data$average.total.playtime)
@@ -14,20 +12,27 @@ data$average.recent.playtime <- sub("h","",data$average.recent.playtime)
 
 data$average.total.playtime <- as.numeric(data$average.total.playtime)
 data$average.recent.playtime <- as.numeric(data$average.recent.playtime)
-
-data <- data[!names(data) %in% "score_rank"]
-data <- data[!names(data) %in% "fix"]
-nrow(data)
-data <- na.omit(data)
-nrow(data)
-names(data)
-no_needed <- c("appid","name","developer","publisher","owners","discount","languages","genre","tags","release_date")
+data$initialprice <- data$initialprice/100
+no_needed <- c("languages","genre","tags","release_date","fix","score_rank","user_socre","price","discount","ccu")
 data <- data[!names(data) %in% no_needed]
+nrow(data)
 
+colnames(data)
+data <- na.omit(data)
+data_free <- data[data$average_price != 0,]
+data_free$owners <- NULL
+
+dim(data_free)
+
+m <- lm(formula = estimated.players ~ review_num+positive+days_from_release+languages_supports+average_cut+duration+average_price,data = data)
+summary(m)
+anova(m)
+colnames(data)
+var(data$estimated.players)
 data <- data %>% relocate(estimated.active.players)
 data <- data %>% relocate(estimated.players)
 names(data)
-
+data$userscore
 
 data %>% count(cut_width(estimated.players, 2445000))
 
@@ -36,13 +41,11 @@ scatterplotMatrix(data)
 
 ep <- data$estimated.players
 
-
-
-max(ep)/20
-
+cor(data$review_num,data$estimated.active.players)
 
 
 
-sample_ <- sample(nrow(data),100)
-data <- data[sample_,]
+ggplot(data=data,mapping = aes(x = estimated.players)) + geom_density() +dark_theme_classic()
+summary(data$estimated.players)
+
 
